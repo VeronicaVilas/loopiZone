bc1006e60c1966ce63ba56ab4f3c051e7d398d02
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-19 20:42:11 -0300

    feat: Adiciona barra de navegação

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}

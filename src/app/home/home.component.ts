1b030fcafa69a28bdf210f40b5bfeb3b9e23a451
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-21 19:31:51 -0300

    refactor: Refatora componente home adicionando barra colpsada e banner e video component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BannerComponent } from './banner/banner.component';
+import { VideoComponent } from './video/video.component';
+import { AutoCollapseSidebarDirective } from '../shared/directive/auto-collapse-sidebar.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [BannerComponent, VideoComponent, CommonModule, AutoCollapseSidebarDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  @Input() sidebarCollapsed: boolean = true;
 }

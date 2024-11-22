12df1ff1af6a2db8700c8f046e0fbc610ebb1672
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-22 15:48:03 -0300

    refactor: remove play-video do app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { PlayVideoComponent } from './play-video/play-video.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent, PlayVideoComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

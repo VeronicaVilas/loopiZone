1a84176169d8108b321c6284ec597e491d3314c9
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-22 15:46:30 -0300

    feat: adiciona rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlayVideoComponent } from './play-video/play-video.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'watch',
+    component: PlayVideoComponent,
+  },
+];

4c322a09f6d7df46321c1d1dde69d41c6941a30e
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-22 22:34:37 -0300

    feat: Adiciona metódo get para listar os vídeos

--- src/app/home/video/video.component.ts
+++ src/app/home/video/video.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+
+import { VideoService } from '../../shared/services/video/video.service';
+import { Video } from '../../shared/interfaces/video';
+import { CommonModule } from '@angular/common';
+import { catchError, empty, Observable, of } from 'rxjs';
+
+import { ShortNumberPipe } from '../../shared/pipes/shortNumber/short-number.pipe';
+import { TimeAgoPipe } from '../../shared/pipes/timeAgo/time-ago.pipe';
 
 @Component({
   selector: 'app-video',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule, ShortNumberPipe, TimeAgoPipe],
   templateUrl: './video.component.html',
   styleUrl: './video.component.css'
 })
-export class VideoComponent {
+export class VideoComponent implements OnInit {
 
+  videos$: Observable<Video[]> = of([]);
+
+  constructor(private videoService: VideoService) {
+  }
+
+  ngOnInit() {
+    this.videos$ = this.videoService.get()
+    .pipe(
+      catchError(error => {
+        console.log(error);
+        return empty();
+      })
+    );
+  }
 }

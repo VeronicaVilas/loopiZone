ac6ebe1870df1600c7faa0f41b4af0b9715b5fab
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-25 01:04:32 -0300

    feat: Adiciona banner reativo conforme a autenticação

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthenticationService } from '../../shared/services/authentication/authentication.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
 
+  isAuthenticated$;
+
+  constructor(private authService: AuthenticationService) {
+    this.isAuthenticated$ = this.authService.isAuthenticated$();
+  }
+
+  login() {
+    this.authService.login();
+  }
 }

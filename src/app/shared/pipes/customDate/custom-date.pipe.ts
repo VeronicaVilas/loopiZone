99c40735c5c15c236b0db80dd2d1e6d76e4eab36
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-25 17:58:02 -0300

    feat: Cria pipe para customizar números

---
+++ src/app/shared/pipes/customDate/custom-date.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'customDate',
+  standalone: true
+})
+export class CustomDatePipe implements PipeTransform {
+
+  transform(value: Date | string | null): string {
+    if (!value) return '';
+
+    const date = new Date(value);
+    const options: Intl.DateTimeFormatOptions = {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    };
+
+    // Formatar usando Intl com a localidade pt-BR
+    return new Intl.DateTimeFormat('pt-BR', options).format(date);
+  }
+}

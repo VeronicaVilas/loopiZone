e0ef3324be91056a432cfbe784cfe44c8729accd
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-24 23:22:48 -0300

    feat: Configurao auth0 na aplicação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -11,5 +12,16 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    provideAuth0({
+      domain: 'dev-eeipn5gto1zg5v42.us.auth0.com',
+      clientId: '9JctXyGk9SUqr5YjgcXF5UfSS6CMSfNg',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+        audience: 'https://dev-eeipn5gto1zg5v42.us.auth0.com/api/v2/',
+        scope: 'openid profile email offline_access',
+      },
+      useRefreshTokens: true,
+      cacheLocation: 'localstorage',
+    }),
   ]
 };

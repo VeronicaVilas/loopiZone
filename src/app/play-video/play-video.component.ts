b56cfd8804056c834b11aa68bddad49ca895845d
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-25 18:00:25 -0300

    feat: Adiciona listagem de videos conforme o id e videos globais

--- src/app/play-video/play-video.component.ts
+++ src/app/play-video/play-video.component.ts
@@ -2,14 +2,49 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { Video } from '../shared/interfaces/video';
+import { ActivatedRoute } from '@angular/router';
+import { VideoService } from '../shared/services/video/video.service';
+import { map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+import { VideoHelperService } from '../shared/services/video/video-helper.service';
+import { CustomNumberPipe } from '../shared/pipes/customNumber/custom-number.pipe';
+import { CustomDatePipe } from '../shared/pipes/customDate/custom-date.pipe';
+import { ShortNumberPipe } from '../shared/pipes/shortNumber/short-number.pipe';
+import { TimeAgoPipe } from '../shared/pipes/timeAgo/time-ago.pipe';
 
 @Component({
   selector: 'app-play-video',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatDividerModule],
+  imports: [MatIconModule, MatButtonModule, MatDividerModule, CommonModule, CustomNumberPipe, CustomDatePipe,  ShortNumberPipe, TimeAgoPipe],
   templateUrl: './play-video.component.html',
   styleUrl: './play-video.component.css'
 })
 export class PlayVideoComponent {
+  video$!: Observable<Video | undefined>;
+  videos$!: Observable<Video[]>;
+  safeUrl: SafeResourceUrl | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private videoService: VideoService,
+    private videoHelper: VideoHelperService,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.video$ = this.videoService.getVideoById(id);
+    this.videos$ = this.videoService.get().pipe(
+      map((videos) => videos.filter((video) => video.id !== id))
+    );
+    this.video$.subscribe((video) => {
+      if (video) {
+        const embedUrl = this.videoHelper.convertToEmbedUrl(video.url);
+        this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
+      }
+    });
+  }
 }

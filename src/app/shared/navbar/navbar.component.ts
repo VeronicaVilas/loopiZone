68a18fbb1c721e796a0c6bc3efc3628d940ca09b
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-28 21:48:17 -0300

    chore: Ajusta estrutura e remove comentários desnecessários

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,17 +1,25 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { SearchService } from '../services/search/search.service';
 import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
+
+import { SearchService } from '../services/search/search.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -20,18 +28,15 @@ export class NavbarComponent {
   searchControl = new FormControl('');
 
   constructor(private searchService: SearchService) {
-    // Observa as mudanças no campo de entrada
     this.searchControl.valueChanges
-      .pipe(
-        debounceTime(300), // Aguarda 300ms entre digitações
-        distinctUntilChanged() // Evita emitir valores repetidos
-      )
-      .subscribe((query) => {
-        // Trata o valor 'null' ou 'undefined' e converte para uma string vazia
-        const searchTerm = query || '';
-        console.log('Query digitada:', searchTerm);
-        this.searchService.setSearchTerm(searchTerm); // Atualiza o termo de pesquisa no serviço
-      });
+    .pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    )
+    .subscribe((query) => {
+      const searchTerm = query || '';
+      this.searchService.setSearchTerm(searchTerm);
+    });
   }
 
   onToggleSidebar() {

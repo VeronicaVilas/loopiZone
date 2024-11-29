371b8898ae94f05eb997b368881b71a1650f0c87
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-11-29 07:10:18 -0300

    chore: ajusta estrutura do guard e remove comentários desnecessários

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -4,14 +4,11 @@ import { AuthService } from '@auth0/auth0-angular';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService);
-
   let isAuthenticated = false;
   auth.isAuthenticated$.subscribe((isAuth) => (isAuthenticated = isAuth));
-
   if (!isAuthenticated) {
     auth.loginWithRedirect();
     return false;
   }
-
   return true;
 };
